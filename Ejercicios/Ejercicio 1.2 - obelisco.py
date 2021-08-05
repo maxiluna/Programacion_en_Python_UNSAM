ee5dccd6a88f28ba281d7010b0f8acf4be2c2471
maxiluna <lunamaximiliano@gmail.com>
2021-08-05 16:41:59 -0300

    Create Ejercicio 1.2 - obelisco.py

---
+++ Ejercicios/Ejercicio 1.2 - obelisco.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  17 16:32:23 2021
+
+@author: maximiliano.luna
+"""
+
+
+# obelisco.py
+
+grosor_billete = 0.11 * 0.001 # 0.11 mm en metros
+altura_obelisco = 67.5         # altura en metros
+num_billetes = 1
+dia = 1
+
+while num_billetes * grosor_billete <= altura_obelisco:
+    print(dia, num_billetes, num_billetes * grosor_billete)
+    dia = dia + 1 # Variable dias no definida
+    num_billetes = num_billetes * 2
+
+print('Cantidad de días', dia)
+print('Cantidad de billetes', num_billetes)
+print('Altura final', num_billetes * grosor_billete)
